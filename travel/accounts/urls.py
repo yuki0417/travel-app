89e56d62a895555d515b8072d79722803b4a386c
岡勇樹 <yuki@Mac.local>
2019-10-16 21:51:34 +0900

    ユーザーのログアウト機能の実装。その他、テストユーザーログイン機能のロジックを改修。また、画面のレイアウトを簡易的に調整。

--- travel/accounts/urls.py
+++ travel/accounts/urls.py
@@ -8,4 +8,7 @@ app_name = 'accounts'
 urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name='signup'),
     path('login/', views.AccountLoginView.as_view(), name='login'),
+    path('test_login/', views.test_login, name='test_login'),
+    path('logout_confirm/', views.logout_confirm, name='logout_confirm'),
+    path('logged_out/', views.logged_out, name='logged_out'),
 ]
