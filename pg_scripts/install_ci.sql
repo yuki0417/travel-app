3acf84adad7a4d1b293335e39b91b9fe6caef068
岡勇樹 <yuki@Mac.local>
2019-10-22 09:18:34 +0900

    CI用DBスクリプトの修正。

--- pg_scripts/install_ci.sql
+++ pg_scripts/install_ci.sql
@@ -1,4 +1,8 @@
--- CREATE ROLE IF NOT EXISTS travel_ci WITH LOGIN PASSWORD 'travel_ci';
+CREATE ROLE travel_ci WITH LOGIN PASSWORD 'travel_ci';
+CREATE DATABASE djangodb_ci WITH TEMPLATE = template0 OWNER = travel_ci ENCODING = 'UTF8' LC_COLLATE = 'ja_JP.UTF-8' LC_CTYPE = 'ja_JP.UTF-8';
+\connect djangodb_ci
+CREATE SCHEMA IF NOT EXISTS django_ci;
+GRANT ALL PRIVILEGES on database djangodb_ci to travel_ci;
 CREATE DATABASE test_djangodb_ci WITH TEMPLATE = template0 OWNER = travel_ci ENCODING = 'UTF8' LC_COLLATE = 'ja_JP.UTF-8' LC_CTYPE = 'ja_JP.UTF-8';
 \connect test_djangodb_ci
 CREATE SCHEMA IF NOT EXISTS django_ci;
