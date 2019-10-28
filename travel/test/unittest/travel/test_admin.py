b00286f99de7dc741408e8713e3338738f67115b
岡勇樹 <yuki@Mac.local>
2019-10-28 21:11:08 +0900

    travel.adminとtravel.context_processorsのテストを実装

---
+++ travel/test/unittest/travel/test_admin.py
@@ -0,0 +1,62 @@
+from django.test import TestCase
+from django.contrib.admin.sites import AdminSite
+
+from travel.models import Setting, Place
+from travel.admin import SettingAdmin, PlaceAdmin
+from test.unittest.common.test_data import (
+    AppUserCorrectTestData1st,
+    SettingCorrectTestData1st,
+    PlaceCorrectTestData1st,
+)
+
+
+class SettingAdminTestcase(TestCase):
+    """
+    設定情報の一覧画面のテスト
+    """
+    databases = '__all__'
+
+    def setUp(self):
+        AppUserCorrectTestData1st.setUp()
+        SettingCorrectTestData1st.setUp()
+        self.site = AdminSite()
+
+    def test_class_variable__is_registered_correctly(self):
+        sa = SettingAdmin(Setting, self.site)
+        list_display = (
+            'user',
+            'name',
+            'radius',
+            'max_show_num',
+        )
+        self.assertEqual(sa.save_as, True)
+        self.assertEqual(sa.list_display, list_display)
+        self.assertEqual(sa.readonly_fields, ('id',))
+
+
+class PlaceAdminTestcase(TestCase):
+    """
+    設定情報の一覧画面のテスト
+    """
+    databases = '__all__'
+
+    def setUp(self):
+        AppUserCorrectTestData1st.setUp()
+        SettingCorrectTestData1st.setUp()
+        PlaceCorrectTestData1st.setUp()
+        self.site = AdminSite()
+
+    def test_class_variable__is_registered_correctly(self):
+        pa = PlaceAdmin(Place, self.site)
+        list_display = (
+            'user',
+            'name',
+            'saved_time',
+            'linkUrl',
+            'imageUrl',
+            'latitude',
+            'longtitude',
+        )
+        self.assertEqual(pa.save_as, True)
+        self.assertEqual(pa.list_display, list_display)
+        self.assertEqual(pa.readonly_fields, ('id',))
