99d0c35c144873ebdd7aff8244ef24f16988851f
岡勇樹 <yuki@Mac.local>
2019-11-05 20:29:49 +0900

    設定更新用のフォームを実装。navbarのjquery読み込みのためのテンプレート整理。

--- travel/test/unittest/travel/test_form.py
+++ travel/test/unittest/travel/test_form.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django import forms
 
 from travel.models import Setting
-from travel.forms import SettingForm
+from travel.forms import SettingForm, SettingUpdateForm
 from test.unittest.common.test_data import (
     COR_APPUSER_DATA_1st,
     COR_SETTING_DATA_1st,
@@ -44,3 +44,21 @@ class SettingFormTestcase(TestCase):
         result = msf.errors['name'][0]
         expect = '同じ設定名が存在します。違う設定名に変更してください。'
         self.assertEqual(result, expect)
+
+
+class SettingUpdateFormTestcase(TestCase):
+    """
+    設定情報の一覧画面のテスト
+    """
+    databases = '__all__'
+
+    def setUp(self):
+        AppUserCorrectTestData1st.setUp()
+        SettingCorrectTestData1st.setUp()
+
+    def test_class_meta_variable__is_registered_correctly(self):
+        meta = SettingUpdateForm(Setting).Meta()
+        fields = ('user', 'name', 'radius', 'max_show_num')
+        self.assertEqual(meta.model, Setting)
+        self.assertEqual(meta.fields, fields)
+        self.assertIsInstance(meta.widgets['user'], forms.HiddenInput)
