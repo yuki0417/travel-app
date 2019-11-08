7caa26864c01c242305ffc5930c8a614a91a7efc
岡勇樹 <yuki@Mac.local>
2019-11-08 19:27:09 +0900

    ログインの統合テストを試験的に実装。chromeドライバーなどの環境を整備。

---
+++ travel/test/integtest/accounts/test_login.py
@@ -0,0 +1,50 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+
+from test.unittest.common.test_data import (
+    AppUserEncPasswordTestData1st,
+    COR_APPUSER_DATA_1st,
+)
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+class MySeleniumTests(StaticLiveServerTestCase):
+
+    def setUp(self):
+        AppUserEncPasswordTestData1st.setUp()
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        chromedriver_path = '/usr/bin/chromedriver'
+        o = Options()
+        o.binary_location = '/usr/bin/chromium'
+        o.add_argument('--headless')
+        o.add_argument('--disable-gpu')
+        o.add_argument('--no-sandbox')
+        o.add_argument('--window-size=1200x600')
+        cls.selenium = webdriver.Chrome(chromedriver_path, options=o)
+        cls.selenium.implicitly_wait(5)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_login__when_success(self):
+        """
+        ログイン画面にアクセスし、必要なフォーム入力し、ログインボタンを押す
+        """
+        self.selenium.get('%s%s' % (self.live_server_url, '/accounts/login/'))
+        username_input = self.selenium.find_element_by_name("username")
+        username_input.send_keys(COR_APPUSER_DATA_1st['username'])
+        password_input = self.selenium.find_element_by_name("password")
+        password_input.send_keys(COR_APPUSER_DATA_1st['password'])
+        self.selenium.find_element_by_xpath(
+            '/html/body/div/div/div/form/div[3]/button').click()
+
+        result = self.selenium.current_url
+        expected = self.live_server_url + '/travel/list/'
+
+        self.assertEqual(result, expected)
