37af1b76a2d7e5ff1acb6194f219858a6d893a7b
yuki0417 <nicorasuster@gmail.com>
2019-12-14 20:35:09 +0900

    静的ファイルをS3に配置する設定。

--- travel/setting/settings.py
+++ travel/setting/settings.py
@@ -12,8 +12,18 @@ ALLOWED_HOSTS = [config('ALLOWED_HOSTS')]
 
 ROOT_URLCONF = 'setting.urls'
 
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+# 静的ファイルの設定
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
+
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # TODO: 本番環境のRDS設定を行う
 DATABASES = {
@@ -25,14 +35,14 @@ DATABASES = {
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
-#        'HOST': 'RDSのエンドポイント名を入れる',
+        'HOST': config('DB_HOST'),
         'PORT': config('DB_PORT'),
     }
 }
 
 # TODO: 本番環境のElastiCache設定を行う
 SESSION_REDIS = {
-#    'host': 'ElastiCacheのエンドポイント名を入れる'',
+    'host': config('REDIS_HOST'),
     'port': 6379,
     'db': 0,
     'password': config('REDIS_PASSWORD'),
